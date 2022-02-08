1f59f4b2b79dd7903cbf3ab485e08bb0290b6056
Erick Osterling <erick.osterling@gmail.com>
2022-02-07 22:32:57 -0500

    Add feature to choose bet amount

--- js/main.js
+++ js/main.js
@@ -2,53 +2,77 @@
 // jugador es mayor de edad con un condicional. 
 
 let tiroRuleta = () => {
-    let numeroRuleta = Math.floor(Math.random()*37) ;
-    return arrayRuleta[numeroRuleta];
+    let numeroRuleta = Math.floor(Math.random()*37);
+    let color = arrayRuleta[numeroRuleta]
+    recordColor.push(color);
+    console.log(recordColor);
+    return color;
 }
 
 let arrayRuleta = ["verde","rojo","negro","rojo","negro","rojo","negro","rojo","negro","rojo","negro","negro","rojo","negro","rojo","negro","rojo","negro","rojo","rojo","negro","rojo","negro","rojo","negro","rojo","negro","rojo","negro","negro","rojo","negro","rojo","negro","rojo","negro","rojo" ];
 
 let eleccionJugador = () => {
-    let eleccion = prompt('Por favor elija un color, ya sea "rojo" o "negro"').toLowerCase();
+    let eleccion = prompt('Por favor elija un color, ya sea "rojo" o "negro" o "verde"').toLowerCase();
     return eleccion;
 }
 
 let edad = prompt("Por favor indique qué edad tiene:");
-let creditosFinales = 0;
-let numeroJugadas = 0;
-let creditosIniciales = 0;
+let numeroTurnos = 0;
+let creditos = 0;
 let contadorGanadas = 0;
+let apuesta = 0;
+let creditosIniciales = 0;
+const recordColor = [];
+let displayCreditos = document.getElementById("creditos");
+
 
 if (edad >= 18) {
     alert ("bienvenido al casino más justo del mundo!")
 
-    numeroJugadas = prompt("¿Cuantos turnos desea jugar?")*1;
-    creditosIniciales = prompt("por favor introduzca los créditos que posee. Esta cantidad debe ser por lo menos igual al número de jugadas")*1;
+    numeroTurnos = prompt("¿Cuantos turnos desea jugar?")*1;
+    creditos = prompt("por favor introduzca los créditos que posee. Esta cantidad debe ser por lo menos igual al número de jugadas")*1;
+    creditosIniciales = creditos;
 
-    if (creditosIniciales< numeroJugadas) {
+    if (creditos < numeroTurnos) {
         alert ("no dispone de créditos suficientes para efectuar el numero de jugadas que ha planeado")
     } else {
 
         alert ("Que empieze el juego!");
-        alert ("Desea iniciar la corrida de apuestas?");
 
-        for (let i = 0; i < numeroJugadas ; i++) {
-            let jugada = eleccionJugador();
-            let ruleteada = tiroRuleta();
-            creditosIniciales--;
-
-            if(jugada === ruleteada) {
-                creditosFinales += 2;
-                contadorGanadas++;
-                alert(`Felicidades, apostó por ${jugada} y salió ${ruleteada}. Ha ganado 1 crédito!`);
+        for (let i = 0; i < numeroTurnos ; i++) {
+            apuesta = prompt("¿Cuantos créditos desea apostar?")*1;
+            if (apuesta > creditos ) {
+                console.log(`apuesta: ${apuesta} y creditos: ${creditos}`);
+                alert("No dispone de esta cantidad créditos. Debe apostar menos cantidad");
+                i--;
+                console.log(i);
+                
             } else {
-                alert(`Lo sentimos. Usted apostó ${jugada} y salió ${ruleteada}. Ha perdido la apuesta`);
+                alert(`Ha apostado ${apuesta} créditos.`);
+                let jugada = eleccionJugador();
+                let ruleteada = tiroRuleta();
+
+                if(jugada === ruleteada) {
+                    alert(`Felicidades, apostó por ${jugada} y salió ${ruleteada}. Ha ganado ${apuesta} créditos!`);
+                    creditos += apuesta;
+                    displayCreditos.innerText = creditos;
+                    console.log(creditos);
+                    contadorGanadas++;
+                    
+                } else {
+                    alert(`Lo sentimos. Usted apostó ${jugada} y salió ${ruleteada}. Ha perdido la apuesta`);
+                    creditos -=apuesta;
+                    displayCreditos.innerText = creditos;
+                    console.log(creditos);
+                }
+                alert (`Le quedan ${(numeroTurnos -1) - i} jugadas`);
+                alert(`Tiene ${creditos} créditos disponibles para su próxima apuesta`);
+
             }
-            alert (`Le quedan ${(numeroJugadas -1) - i} jugadas`);
         }
-        
-        alert(`El juego acabó. Usted ganó ${contadorGanadas} veces y perdió ${numeroJugadas - contadorGanadas} veces`);
-        alert(`Su saldo final es ${creditosIniciales + creditosFinales}`);
+        alert(`El juego acabó. Usted ganó ${contadorGanadas} veces y perdió ${numeroTurnos - contadorGanadas} veces`);
+        alert(`Su saldo final es ${creditos}`);
+
     }
 
 } else {
